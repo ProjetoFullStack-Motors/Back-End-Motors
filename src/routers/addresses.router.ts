d3d2758dbd242c5592496e5d0ebccb0d427f5c0c
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-03 09:05:42 -0300

    feat: add inital routes files

---
+++ src/routers/addresses.router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const addresses = Router();
+
+export default addresses;
---
+++ src/routers/index.ts
@@ -0,0 +1,13 @@
+import addresses from "./addresses.router";
+import orders from "./orders.router";
+import salesAd from "./salesAd.router";
+import users from "./users.router";
+
+const routes = {
+	addresses,
+	orders,
+	salesAd,
+	users,
+};
+
+export default routes;
