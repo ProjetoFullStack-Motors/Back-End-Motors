9bc0f5a68f0bea9423a5ea39cb2226514cd51eac
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-03 09:02:16 -0300

    feat: add initial middlewares files

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,15 @@
 import shared from "./shared";
+import addresses from "./addresses";
+import orders from "./orders";
+import salesAd from "./salesAd";
+import users from "./users";
 
 const middlewares = {
 	...shared,
+	...addresses,
+	...orders,
+	...salesAd,
+	...users
 };
 
 export default middlewares;
---
+++ src/middlewares/salesAd/index.ts
@@ -0,0 +1,3 @@
+const salesAd = {};
+
+export default salesAd;
