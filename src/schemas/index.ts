b31aa9e1eb4e4b28835c4ce0d87b8d0154dd1592
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-03 09:09:20 -0300

    feat: add inital schemas files

---
+++ src/schemas/index.ts
@@ -0,0 +1,13 @@
+import addresses from "./addresses.schemas";
+import orders from "./orders.schemas";
+import salesAd from "./salesAd.schemas";
+import users from "./users.schemas";
+
+const schemas = {
+	addresses,
+	users,
+	orders,
+	salesAd
+};
+
+export default schemas;
