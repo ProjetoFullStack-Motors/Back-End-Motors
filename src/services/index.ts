20f06f6b35a40923ea918ff4d49a676474860325
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-03 09:12:49 -0300

    feat: add inital services files

---
+++ src/services/index.ts
@@ -0,0 +1,13 @@
+import addresses from "./addresses";
+import orders from "./orders";
+import salesAd from "./salesAd";
+import users from "./users";
+
+const services = {
+	addresses,
+	users,
+	orders,
+	salesAd
+};
+
+export default services;
