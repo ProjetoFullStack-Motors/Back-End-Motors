3573a9f321e259c4a606056d29e352d0cd4b1319
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-07 11:41:58 -0300

    feat(app): implement salesAd router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import "reflect-metadata";
 import "express-async-errors";
 import cors from "cors";
 
+import routes from "./routers";
 import middlewares from "./middlewares";
 
 const app = express();
 app.use(json());
 app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use("/salesAd", routes.salesAd);
 
 app.use(middlewares.handleError);
 
