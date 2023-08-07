782816b997bb5f2896194f5b60c0ecdf6bb43bd6
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-07 15:27:21 -0300

    fix(salesAd): correcting service deleteById

--- src/services/salesAd/deleteSalesAd.service.ts
+++ src/services/salesAd/deleteSalesAd.service.ts
@@ -2,11 +2,11 @@ import { SalesAd } from "../../entities/salesAd.entity";
 import repositories from "../../utils";
 
 export const deleteById = async (salesAdId: string): Promise<void> => {
-  const salesAd: SalesAd | null = await repositories.salesAdRepo.findOneBy({
-    id: salesAdId,
-  });
+    const salesAd: SalesAd | null = await repositories.salesAdRepo.findOneBy({
+        id: salesAdId,
+    });
 
-  await repositories.salesAdRepo.remove(salesAd);
+    await repositories.salesAdRepo.remove(salesAd!);
 
-  return;
+    return;
 };
