c95bbc3ec1552cdb5e2b22f8ec9a69c3488bf080
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-07 12:15:50 -0300

    feat(salesAd): add controllers with initial functions

--- src/controllers/salesAd.controller.ts
+++ src/controllers/salesAd.controller.ts
@@ -1,5 +1,41 @@
-const salesAd = {
+import { Request, Response } from "express";
 
+const create = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("create controller");
+};
+
+const readAll = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("readAll controller");
+};
+
+const readById = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("readById controller");
+};
+
+const updateById = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("updateById controller");
+};
+
+const deleteById = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("deleteById controller");
+};
+
+const createImage = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("createImage controller");
+};
+
+const updateImageById = async (req: Request, res: Response): Promise<Response> => {
+	return await res.json("updateImageById controller");
+};
+
+const salesAd = {
+	create,
+	readAll,
+	readById,
+	updateById,
+	deleteById,
+	createImage,
+	updateImageById
 };
 
 export default salesAd;
