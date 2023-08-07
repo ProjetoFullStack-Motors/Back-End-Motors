f525a74f0f0bd3cf0e8ab60fb077c16455a8170c
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-07 14:47:24 -0300

    feat(salesAd): implement service deleteById

--- src/services/salesAd/deleteSalesAd.service.ts
+++ src/services/salesAd/deleteSalesAd.service.ts
@@ -1,3 +1,12 @@
+import { SalesAd } from "../../entities/salesAd.entity";
+import repositories from "../../utils";
+
 export const deleteById = async (salesAdId: string): Promise<void> => {
-    
+  const salesAd: SalesAd | null = await repositories.salesAdRepo.findOneBy({
+    id: salesAdId,
+  });
+
+  await repositories.salesAdRepo.remove(salesAd);
+
+  return;
 };
