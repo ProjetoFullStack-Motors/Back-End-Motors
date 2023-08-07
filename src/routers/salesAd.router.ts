b347c7c0d2db3f776c345e3d654e62f485343b9d
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-07 11:50:16 -0300

    feat(salesAd): implement routes

--- src/routers/salesAd.router.ts
+++ src/routers/salesAd.router.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
 
-const salesAd = Router();
+const salesAd: Router = Router();
+
+salesAd.post("", () =>  console.log("salesAd post"));
+salesAd.get("", () =>  console.log("salesAd get"));
+salesAd.get("/:id", () =>  console.log("salesAd get by id"));
+salesAd.patch("/:id", () =>  console.log("salesAd post"));
+salesAd.delete("/:id", () =>  console.log("salesAd delete"));
+
+salesAd.post("/:salesAdId/images", () =>  console.log("salesImages post"));
+salesAd.patch("/:salesAdId/images", () =>  console.log("salesImages patch"));
 
 export default salesAd;
