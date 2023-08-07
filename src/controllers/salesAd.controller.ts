cb92b3cc9f63d57bcc38b9b093156e757adccd3e
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-07 15:55:03 -0300

    feat(salesAd): implement controller deleteById

--- src/controllers/salesAd.controller.ts
+++ src/controllers/salesAd.controller.ts
@@ -1,41 +1,49 @@
 import { Request, Response } from "express";
+import salesAdServices from "../services/salesAd";
 
 const create = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("create controller");
+    return await res.json("create controller");
 };
 
 const readAll = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("readAll controller");
+    return await res.json("readAll controller");
 };
 
 const readById = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("readById controller");
+    return await res.json("readById controller");
 };
 
 const updateById = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("updateById controller");
+    return await res.json("updateById controller");
 };
 
 const deleteById = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("deleteById controller");
+    const salesAdId: string = req.params.id;
+
+    await salesAdServices.deleteById(salesAdId);
+
+    return res.status(204).send();
 };
 
 const createImage = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("createImage controller");
+    return await res.json("createImage controller");
 };
 
-const updateImageById = async (req: Request, res: Response): Promise<Response> => {
-	return await res.json("updateImageById controller");
+const updateImageById = async (
+    req: Request,
+    res: Response
+): Promise<Response> => {
+    return await res.json("updateImageById controller");
 };
 
 const salesAd = {
-	create,
-	readAll,
-	readById,
-	updateById,
-	deleteById,
-	createImage,
-	updateImageById
+    create,
+    readAll,
+    readById,
+    updateById,
+    deleteById,
+    createImage,
+    updateImageById,
 };
 
 export default salesAd;
