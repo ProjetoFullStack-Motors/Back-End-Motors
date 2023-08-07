d2c27e07feba52308b7539b0d30a21ded260bca6
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-07 16:20:48 -0300

    feat(salesAd): implement middleware on the route delete salesAd

--- src/routers/salesAd.router.ts
+++ src/routers/salesAd.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import controllers from "../controllers";
+import middlewares from "../middlewares";
 
 const salesAd: Router = Router();
 
@@ -7,7 +8,11 @@ salesAd.post("", controllers.salesAd.create);
 salesAd.get("", controllers.salesAd.readAll);
 salesAd.get("/:id", controllers.salesAd.readById);
 salesAd.patch("/:id", controllers.salesAd.updateById);
-salesAd.delete("/:id", controllers.salesAd.deleteById);
+salesAd.delete(
+    "/:id",
+    middlewares.existsSalesAdId,
+    controllers.salesAd.deleteById
+);
 
 salesAd.post("/:id/images", controllers.salesAd.createImage);
 salesAd.patch("/:id/images/:imageId", controllers.salesAd.updateImageById);
