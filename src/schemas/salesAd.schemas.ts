2a54b5da894927211d9a8aa2fc75e95375390263
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-07 10:21:21 -0300

    feat(salesAd): implement schemas

--- src/schemas/salesAd.schemas.ts
+++ src/schemas/salesAd.schemas.ts
@@ -1,3 +1,28 @@
-const salesAd = {};
+import { z } from "zod";
+
+const response = z.object({
+	id: z.string(),
+	brand: z.string(),
+	model: z.string(),
+	year: z.string(),
+	mileage: z.number(),
+	engine: z.enum(["flex", "hybrid", "electric"]),
+	isGoodPrice: z.boolean(),
+	price: z.number(),
+	color: z.string(),
+	description: z.string(),
+	status: z.boolean(),
+	createdAt: z.string(),
+});
+
+const request = response.omit({ id: true, createdAt: true });
+
+const responseArray = response.array();
+
+const salesAd = {
+	response,
+	request,
+	responseArray
+};
 
 export default salesAd;
