220b8fbf27685a877a5d3611aeedfd38b986a7ad
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-08 13:18:41 -0300

    fix(salesAd): correcting update interface salesAd

--- src/interfaces/salesAd.interface.ts
+++ src/interfaces/salesAd.interface.ts
@@ -5,11 +5,11 @@ import schemas from "../schemas";
 
 type TSalesAdResponse = z.infer<typeof schemas.salesAd.response>;
 
-type TSalesAdResponseArray = z.infer<typeof schemas.salesAd.responseArray>
+type TSalesAdResponseArray = z.infer<typeof schemas.salesAd.responseArray>;
 
 type TSalesAdRequest = z.infer<typeof schemas.salesAd.request>;
 
-type TSalesAdUpdate = DeepPartial<typeof schemas.salesAd.update>;
+type TSalesAdUpdate = DeepPartial<TSalesAdRequest>;
 
 type TSalesImagesResponse = z.infer<typeof schemas.salesAd.imagesResponse>;
 
