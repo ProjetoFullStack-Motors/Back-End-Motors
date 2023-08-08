c69084d8cfb68dc2cb733ce437fec5bbea9e559d
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-08 17:18:57 -0300

    fix(database | salesAd): change year field to be string with numbers regex and min/max 4 length and put default value for images princiapl field as false

--- src/database/migrations/1691525711937-createTables.ts
+++ src/database/migrations/1691525711937-createTables.ts
@@ -1,13 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTables1691508624537 implements MigrationInterface {
-    name = 'CreateTables1691508624537'
+export class CreateTables1691525711937 implements MigrationInterface {
+    name = 'CreateTables1691525711937'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "public"."salesAd_engine_enum" AS ENUM('flex', 'hybrid', 'electric')`);
-        await queryRunner.query(`CREATE TABLE "salesAd" ("id" character varying NOT NULL, "created_at" text NOT NULL, "brand" character varying(255) NOT NULL, "model" character varying(255) NOT NULL, "color" character varying(255) NOT NULL, "engine" "public"."salesAd_engine_enum" NOT NULL, "description" text NOT NULL, "year" date NOT NULL, "mileage" integer NOT NULL, "price" integer NOT NULL, "status" boolean NOT NULL DEFAULT true, "isGoodPrice" boolean NOT NULL, CONSTRAINT "PK_5e76654310265ee7cbf2a0c8cdf" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "salesAd" ("id" character varying NOT NULL, "created_at" text NOT NULL, "brand" character varying(255) NOT NULL, "model" character varying(255) NOT NULL, "color" character varying(255) NOT NULL, "engine" "public"."salesAd_engine_enum" NOT NULL, "description" text NOT NULL, "year" character varying(4) NOT NULL, "mileage" integer NOT NULL, "price" integer NOT NULL, "status" boolean NOT NULL DEFAULT true, "isGoodPrice" boolean NOT NULL, CONSTRAINT "PK_5e76654310265ee7cbf2a0c8cdf" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "salesImages" ("id" character varying NOT NULL, "created_at" text NOT NULL, "imageUrl" text NOT NULL, "principal" boolean NOT NULL, "salesAdId" character varying, CONSTRAINT "PK_da26936c1ff5c5508c1b4d8922c" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "salesImages" ADD CONSTRAINT "FK_8547cba5227f7d0c7bcaec89c49" FOREIGN KEY ("salesAdId") REFERENCES "salesAd"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "salesImages" ADD CONSTRAINT "FK_8547cba5227f7d0c7bcaec89c49" FOREIGN KEY ("salesAdId") REFERENCES "salesAd"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
