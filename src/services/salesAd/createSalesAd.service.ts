bbcbcaa60bb17d5ccf34f55d0063c2a34c4193bf
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-08 16:22:57 -0300

    feat(salesAd): implement cents multiplactions valeu for price in create

--- src/services/salesAd/createSalesAd.service.ts
+++ src/services/salesAd/createSalesAd.service.ts
@@ -6,6 +6,11 @@ import repositories from "../../utils";
 export const create = async (
     salesAdData: TSalesAdRequest
 ): Promise<SalesAd> => {
+    salesAdData = {
+        ...salesAdData,
+        price: salesAdData.price * 100
+    };
+
     const sales: SalesAd = repositories.salesAdRepo.create(salesAdData);
 
     await repositories.salesAdRepo.save(sales);
