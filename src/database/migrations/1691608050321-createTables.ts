2d273ea0be2facd77a5b3a3640d0a10515acf3e5
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-09 16:10:33 -0300

    fix(database): unify migrations

---
+++ src/database/migrations/1691608050321-createTables.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTables1691608050321 implements MigrationInterface {
+    name = 'CreateTables1691608050321'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "salesAd" ("id" character varying NOT NULL, "created_at" text NOT NULL, "brand" character varying(255) NOT NULL, "model" character varying(255) NOT NULL, "color" character varying(255) NOT NULL, "engine" character varying(255) NOT NULL, "description" text NOT NULL, "year" character varying(4) NOT NULL, "mileage" integer NOT NULL, "price" integer NOT NULL, "status" boolean NOT NULL DEFAULT true, "isGoodPrice" boolean NOT NULL, CONSTRAINT "PK_5e76654310265ee7cbf2a0c8cdf" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "salesImages" ("id" character varying NOT NULL, "created_at" text NOT NULL, "imageUrl" text NOT NULL, "principal" boolean NOT NULL DEFAULT false, "salesAdId" character varying, CONSTRAINT "PK_da26936c1ff5c5508c1b4d8922c" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "salesImages" ADD CONSTRAINT "FK_8547cba5227f7d0c7bcaec89c49" FOREIGN KEY ("salesAdId") REFERENCES "salesAd"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "salesImages" DROP CONSTRAINT "FK_8547cba5227f7d0c7bcaec89c49"`);
+        await queryRunner.query(`DROP TABLE "salesImages"`);
+        await queryRunner.query(`DROP TABLE "salesAd"`);
+    }
+
+}
