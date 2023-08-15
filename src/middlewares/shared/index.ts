08634f56351f82f9e375f8b7e6271fb7c1d8cdfa
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-15 12:26:31 -0300

    feat: implements ensureAuth middleware

--- src/middlewares/shared/index.ts
+++ src/middlewares/shared/index.ts
@@ -1,9 +1,11 @@
 import { handleError } from "./handlerErrors.middleware";
 import validateSchema from "./validateSchema.middleware";
+import ensureAuth from "./ensureAuth.middleware";
 
 const sharedMiddlewares = {
-	handleError,
-	validateSchema,
+    handleError,
+    validateSchema,
+    ensureAuth,
 };
 
 export default sharedMiddlewares;
